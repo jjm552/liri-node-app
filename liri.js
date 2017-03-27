ac220fd7ad7aad4c500707bdb9a38f7bb9f89366
Jeff McMahan <Jeff McMahan>
2017-03-27 11:44:26 -0500

    twitter option functioning, will show last 20 tweets with time stamp

--- liri.js
+++ liri.js
@@ -10,23 +10,36 @@ var nodeArgs = process.argv;
 
 // twitter option
 if (nodeArgs[2] == "my-tweets") {
-    console.log("TWITTER!");
-    console.log(keys.twitterKeys.consumer_key);
     var client = new twitter({
         consumer_key: keys.twitterKeys.consumer_key,
         consumer_secret: keys.twitterKeys.consumer_secret,
-        bearer_toke: keys.twitterKeys.access_token_key
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
     });
+    var params = {
+        screen_name: '@mcmahan_jeff2',
+        count: '20'
+    };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log("My last 20 tweets!:")
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text + tweets[i].created_at);
+            }
+        }
+    });
+
     // spotify option
 } else if (nodeArgs[2] == "spotify-this-song") {
     var song = nodeArgs[3];
     console.log("SPOTIFY!" + song);
+
     // omdb option
 } else if (nodeArgs[2] == "movie-this") {
     var movie = nodeArgs[3];
     console.log("OMDB!" + movie);
+
     // do what it says option
 } else if (nodeArgs[2] == "do-what-it-says") {
     console.log("FS THIS!");
-
 }
