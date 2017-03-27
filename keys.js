dea5b229dff1442291994c5bca76e0a14836faa9
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-03-26 21:27:46 -0500

    keys added

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'bqzQwW8TBxXzCxUW6DTcv8rah',
+  consumer_secret: 'pTCqZuHSzyqA0wCliphN4hyrlpEJcbMfMzb4iZ6tvGQjOzSpn5',
+  access_token_key: '846182174260936706-BBC65BQTtLGfThFAk6j1sMYkpkyxOT0',
+  access_token_secret: 'TD1uJ8nustT6ZXh91wJrgQg7zsROIp3vsrckBAN0lP3kq',
+}
