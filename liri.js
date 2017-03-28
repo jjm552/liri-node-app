6b8b87a71758ebd2ce64e0cf48f84fe9e60da0ed
Jeff McMahan <Jeff McMahan>
2017-03-28 16:40:40 -0500

    omdb code added & obdm npm added to package.json

--- liri.js
+++ liri.js
@@ -1,13 +1,14 @@
-// twitter,spotify & require NPM packages
+// twitter,spotify,require & fs NPM packages
 var twitter = require("twitter");
 var keys = require("./keys.js");
 var spotify = require("spotify");
+var omdb = require("omdb");
 var request = require("request");
 var fs = require("fs");
 
 // Taking in command line arguments
 var nodeArgs = process.argv;
-
+//----------------------------------------------------------------------------------
 // twitter option
 if (nodeArgs[2] == "my-tweets") {
     var client = new twitter({
@@ -28,7 +29,7 @@ if (nodeArgs[2] == "my-tweets") {
             }
         }
     });
-
+    //----------------------------------------------------------------------------------
     // spotify option
 } else if (nodeArgs[2] == "spotify-this-song") {
     var song = nodeArgs[3];
@@ -47,12 +48,28 @@ if (nodeArgs[2] == "my-tweets") {
     //     console.log(data);
     // });
     console.log("SPOTIFY!" + song);
-
+    // -----------------------------------------------------------------------------------
     // omdb option
 } else if (nodeArgs[2] == "movie-this") {
     var movie = nodeArgs[3];
+    var noMovie = "Mr. Nobody";
+    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&plot=full";
+    omdb.get({ title: movie }, true, function(err, movie) {
+        if (err) {
+            return console.error(err);
+        }
+        if (movie == "undefined") {
+            omdb.get({ title: 'Mr. Nobody' }, true, function(err, movie) {
+                console.log('%f (%d) %d/10', movie.title, movie.year, movie.imdb.rating, movie.Country, movie.Language);
+                console.log(movie.plot);
+                console.log(movie.Country);
+            });
+        }
+        console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating, movie.Country, movie.Language);
+        console.log(movie.plot);
+    });
     console.log("OMDB!" + movie);
-
+    //--------------------------------------------------------------------------------------
     // do what it says option
 } else if (nodeArgs[2] == "do-what-it-says") {
     console.log("FS THIS!");
